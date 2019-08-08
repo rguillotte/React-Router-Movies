3caae731b03518897b9c7bb6930ced79b0b5df98
Ray Guillotte <rayguillotte@Rays-Air.fios-router.home>
2019-08-07 22:09:45 -0400

    added Routes and gave functionality to the movie cards

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -2,23 +2,24 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Movie = (props) => {
-  const [movie, setMovie] = useState({});
+  const [movie, setMovie] = useState();
  
   useEffect(() => {
-    const id = 1;
+    const id = props.match.params.id;
     // change ^^^ that line and grab the id from the URL
     // You will NEED to add a dependency array to this effect hook
 
        axios
         .get(`http://localhost:5000/api/movies/${id}`)
         .then(response => {
+          console.log(response.data);
           setMovie(response.data);
         })
         .catch(error => {
           console.error(error);
         });
 
-  },[]);
+  },[movie]);
   
   // Uncomment this only when you have moved on to the stretch goals
   // const saveMovie = () => {
